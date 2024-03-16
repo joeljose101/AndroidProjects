b772190d777d661afb9495ad792403ddb17696bd
joeljose101 <76038274+joeljose101@users.noreply.github.com>
2024-03-16 18:29:38 +0530

    Updated HelloWorld.java by including sum

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,10 @@ import java.io.*;
 
 public class HelloWorld{
   public static void main(String[] args){
+    int sum=0;
+    int a=12,b=38;
+    sum=a+b;
+    System.out.println("Sum is: ",sum);
     System.out.println("Hello World");
   }
 }
